ac00aa1f4cb081fd75f477f875dbf98fbaa58a14
Jacques <jacques.devuono@gmail.com>
2020-03-11 13:43:39 +0100

    errors ok

--- app/components/SignUp/MailForm.vue
+++ app/components/SignUp/MailForm.vue
@@ -4,6 +4,8 @@
         <TextField v-model="email" hint="Votre adresse e-mail" class="lastField"/>
 
         <Button text="Continuer" @tap="next" class="btnContinue" :isEnabled="email != null"/>
+
+        <Label :text="error" v-if="error" class="error"></Label>
     </stack-layout>
 </template>
 
@@ -11,9 +13,20 @@
     export default {
         name:"MailForm",
         props:["email"],
+        data(){
+            return{
+                error:null,
+                regexMail: new RegExp("^[^\\W][a-zA-Z0-9_]+(\\.[a-zA-Z0-9_]+)*\\@[a-zA-Z0-9_]+(\\.[a-zA-Z0-9_]+)*\\.[a-zA-Z]{2,4}$")
+            }
+        },
         methods:{
             next(){
-                this.$bus.$emit("secondStepComplete",this.email)
+                if(this.regexMail.test(this.email)){
+                    this.$bus.$emit("secondStepComplete",this.email)
+                }
+                else{
+                    this.error = "E-mail invalide !";
+                }
             }
         }
     }
